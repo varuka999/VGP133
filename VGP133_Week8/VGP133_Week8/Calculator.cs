3bcc09a310a734f71cd9c1878c2b8c5e10f2360b
varuka999 <vjkpokemon@yahoo.com>
2025-05-29 12:17:08 -0700

    [Week 8] Classwork

---
+++ VGP133_Week8/VGP133_Week8/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace VGP133_Week8
+{
+    internal class Calculator<T>
+    {
+        //public T Add(T a, T b)
+        //{
+        //    T c = a + b;
+        //    return c; 
+        //}
+
+        delegate T Calculate(T a, T b);
+    }
+}
---
+++ VGP133_Week8/VGP133_Week8/Program.cs
@@ -0,0 +1,9 @@
+﻿using VGP133_Week8;
+
+ClassWork classWork = new ClassWork();
+
+//classWork.Delegates();
+//classWork.Actions();
+//classWork.Func();
+//classWork.Lambdas();
+classWork.Practice2();
